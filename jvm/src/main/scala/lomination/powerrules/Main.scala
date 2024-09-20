ce678009cf441934fdbc1936551e47de62724374
lomination <lomination1@gmail.com>
2024-09-20 11:58:21 +0000

    Improve ScalaJVM main function

--- jvm/src/main/scala/lomination/powerrules/Main.scala
+++ jvm/src/main/scala/lomination/powerrules/Main.scala
@@ -7,8 +7,17 @@ import lomination.powerrules.Compiler
 
 @main
 def main(fileName: String) =
+
+  val newFileName =
+    val withExtension = """^[\S\s]+?\.(?:[pP][oO][wW][eE][rR][rR][uU][lL][eE][sS]|[tT][xX][tT])$"""
+    if (fileName.matches(withExtension))
+      fileName.replaceAll("""\.[^.]+$""", ".rules")
+    else
+      fileName+".rules"
+
   val result = for {
     input  <- Using(Source.fromFile(fileName))(_.mkString)
     output <- Compiler(input)
-    _      <- Using(new PrintWriter("exemple.rules"))(_.write(output))
+    _ <- Using(new PrintWriter(newFileName))(_.write(output))
   } yield ()
+  result.get
