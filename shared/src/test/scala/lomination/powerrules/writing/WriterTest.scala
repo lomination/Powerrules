50e672f4bfe82337561ddf2a198901cda6a5498f
lomination <lomination@rejna.org>
2025-07-16 21:48:13 +0000

    Fix comment writing and add a test

---
+++ shared/src/test/scala/lomination/powerrules/writing/WriterTest.scala
@@ -0,0 +1,17 @@
+package lomination.powerrules.writing
+
+import lomination.powerrules.ast.Comment
+import lomination.powerrules.config.Config
+import lomination.powerrules.writing.Writer.given_Writable_Comment.write
+import lomination.powerrules.FunSuite
+
+class WriterTest extends FunSuite {
+
+  test("WriterTest - write comment") {
+    val ast = Comment(" hello")
+    val test = ast.write(using Config())
+    val expected = "# hello\n"
+    assert(clue(test) == clue(expected))
+  }
+
+}
