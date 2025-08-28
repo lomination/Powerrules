2848307efcd502a51bad8297f5bfd07810dd58ea
lomination <lomination@rejna.org>
2025-08-28 17:00:00 +0000

    Add logs and documentation for the comment handler

--- shared/src/main/scala/lomination/powerrules/lexing/CommentHandler.scala
+++ shared/src/main/scala/lomination/powerrules/lexing/CommentHandler.scala
@@ -5,10 +5,24 @@ import lomination.powerrules.lexing.tokens._
 import scala.util.Try
 
 object CommentHandler extends TokenParser {
+
+  /** Removes all comments (single line and multi line comments) from the given token sequence. Note that single line comments do not include the new
+    * line that determines their end.
+    *
+    * @param tokens
+    *   the input token sequence.
+    * @return
+    *   a token sequence where all comments have been removed.
+    */
   def apply(tokens: Seq[Token]): Try[Seq[Token]] =
+    logger.trace("Starting comment handling...")
     parse(mainParser, TokenReader(tokens)) match
-      case Success(tokens, _)     => scala.util.Success(tokens)
-      case NoSuccess.I(msg, next) => scala.util.Failure(Exception(msg + s" at ${next.pos}"))
+      case Success(tokens, _) =>
+        logger.trace("Comment handling succeeded")
+        scala.util.Success(tokens)
+      case NoSuccess.I(msg, next) =>
+        logger.trace(s"Comment handling failed at ${next.pos} due to:\n$msg")
+        scala.util.Failure(Exception(msg + s" at ${next.pos}"))
 
   lazy val mainParser: P[Seq[Token]] =
     rep(singleLineComment | multiLineComment | anyTk)
@@ -16,7 +30,9 @@ object CommentHandler extends TokenParser {
 
   lazy val singleLineComment: P[Unit] =
     slashTk ~ slashTk ~! rep(not(newlineTk) ~ anyTk) ^^^ { () }
+      |< "single line comment"
 
   lazy val multiLineComment: P[Unit] =
     slashTk ~ starTk ~! rep(not(starTk ~ slashTk) ~ anyTk) ~ (starTk ~ slashTk |< "multi line comment end `*/`") ^^^ { () }
+      |< "multi line comment"
 }
